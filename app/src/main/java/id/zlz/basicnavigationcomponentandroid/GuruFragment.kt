cf306c7fc7e88d2f8e69c3ce4b033d9d2fa96c8f
efrizalnf <ondabuzz9@gmail.com>
2021-10-06 21:15:21 +0700

    added tab layout

--- app/src/main/java/id/zlz/basicnavigationcomponentandroid/GuruFragment.kt
+++ app/src/main/java/id/zlz/basicnavigationcomponentandroid/GuruFragment.kt
@@ -7,23 +7,42 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import id.zlz.basicnavigationcomponentandroid.databinding.FragmentGuruBinding
 
 
 class GuruFragment : Fragment() {
-    private lateinit var _binding : FragmentGuruBinding
+    private lateinit var _binding: FragmentGuruBinding
     private val binding get() = _binding
     private lateinit var viewPager2: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-     _binding = FragmentGuruBinding.inflate(layoutInflater, container, false)
-        viewPager2 = binding.pager
+        _binding = FragmentGuruBinding.inflate(layoutInflater, container, false)
 
+
+        viewPager2 = binding.pager
         val pageradapter = ScreenSlidePagerAdapter(this)
         viewPager2.adapter = pageradapter
+
+
+        tabLayout = binding.tabLayout
+        TabLayoutMediator(
+            tabLayout, viewPager2, TabLayoutMediator.TabConfigurationStrategy() { tab, position ->
+                when (position) {
+                    0 -> {
+                        tab.text = "Administrasi"
+                    }
+                    1 -> {
+                        tab.text = "Literasi"
+                    }
+                }
+            }).attach()
+
         return binding.root
     }
 
--- app/src/main/java/id/zlz/basicnavigationcomponentandroid/ScreenSlidePagerAdapter.kt
+++ app/src/main/java/id/zlz/basicnavigationcomponentandroid/ScreenSlidePagerAdapter.kt
@@ -10,22 +10,13 @@ class ScreenSlidePagerAdapter(fa: Fragment) : FragmentStateAdapter(fa) {
     override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
-        var fragment : Fragment? = null
-        when(position){
-            0-> fragment = HomeFragment()
-            1-> fragment = CategoryFragment()
-
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = CategoryFragment()
         }
 
         return fragment as Fragment
-
-//        val fragment = GuruFragment()
-//        fragment.arguments = Bundle().apply {
-//            // Our object is just an integer :-P
-//            putInt("object", position + 1)
-//        }
-//        return fragment
     }
 
-//    override fun createFragment(position: Int): Fragment = GuruFragment()
 }
